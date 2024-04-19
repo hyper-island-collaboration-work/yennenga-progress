bfec72edab8da3ab86f33a6b1384c2ee5f9951f0
marianordqvist <marianordqvist@live.se>
2024-04-19 08:36:59 +0200

    add responsiveness

--- src/components/NewsSection.jsx
+++ src/components/NewsSection.jsx
@@ -1,18 +1,56 @@
+import ButtonLink from "./ButtonLink";
+
 export default function NewsSection() {
   return (
-    <section className="news-section base flex h-80 flex-row justify-between bg-zinc-100 py-0">
-      <div className="flex-item-1">
-        <h3 className="text-2xl">News</h3>
+    <section className="news-section flex flex-col justify-between bg-zinc-100 p-12 md:flex-row">
+      <div className="flex-item-1 flex flex-row items-center justify-between md:items-start">
+        <h3 className="mb-10 font-plexSerif text-2xl font-bold">News</h3>
+        <div className="all-news-link-top pb-7 md:hidden">
+          <a className="text-1xl font-plexSerif font-bold">All news</a>
+        </div>
       </div>
-      <div className="flex-item-2 w-3/5 text-base">
-        <ul>
-          <li className="mt-2">
+      <div className="flex-item-2 text-base md:w-2/4">
+        <ul className="divide-y-[2px] divide-gray-400 border-t-[2px] border-gray-400">
+          <li className="py-6">
             <a>
-              <p className="text-umber font-bold">Title of article</p>
-              <p>Subtitle</p>
+              <p className="mb-2 font-plexSerif font-bold text-umber">
+                Lorem ipsum dolor sit, amet consectetur
+              </p>
+              <p className="font-plexSerif">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Lorem
+                ipsum dolor sit.
+              </p>
+            </a>
+          </li>
+          <li className="py-6">
+            <a>
+              <p className="mb-2 font-plexSerif font-bold text-umber">
+                Lorem ipsum dolor sit, amet consectetur
+              </p>
+              <p className="font-plexSerif">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Lorem
+                ipsum dolor sit.
+              </p>
+            </a>
+          </li>
+          <li className="py-6">
+            <a>
+              <p className="mb-2 font-plexSerif font-bold text-umber">
+                Lorem ipsum dolor sit, amet consectetur
+              </p>
+              <p className="font-plexSerif">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Lorem
+                ipsum dolor sit.
+              </p>
             </a>
           </li>
         </ul>
+
+        <div className="all-news-link-bottom pt-7">
+          <a className="text-1xl hidden font-plexSerif font-bold md:block">
+            All news
+          </a>
+        </div>
       </div>
     </section>
   );
