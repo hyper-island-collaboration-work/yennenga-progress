da7e0683efcf88886db97e4ef66a4b0e2826e235
Jian Huang <143398249+Jianlearnstocode@users.noreply.github.com>
2024-04-21 23:15:41 +0200

    Add media query to buttonLink for text size on mobile, add buttonlink to footer and readyToTake action components (#6)

--- src/components/ReadyToTakeAction.jsx
+++ src/components/ReadyToTakeAction.jsx
@@ -1,24 +1,25 @@
+import ButtonLink from "./ButtonLink";
 import RightArrowOneIcon from "./Icons/RightArrowOneIcon";
 
 export default function ReadyToTakeAction() {
   return (
-    <section className="bg-light border p-10 pt-24">
+    <section className="border bg-light p-10 pt-24">
       <div>
         <h3 className="pr-10 font-montserrat text-3xl font-medium md:pr-0 lg:text-5xl 2xl:text-[80px]">
           Ready to take action?
         </h3>
         <div className="space-y-5 py-16 text-sm md:text-base">
           <div>
-            <button className="text-light flex w-fit items-center gap-2 rounded-xl bg-sand p-1 font-plexSerif">
-              <p className="text-xs">For Individual Donor</p>
+            <div className="flex w-fit items-center gap-2 rounded-xl bg-sand p-1 font-plexSerif text-light">
+              <ButtonLink buttonName={"For Individual Donor"} />
               <RightArrowOneIcon />
-            </button>
+            </div>
           </div>
           <div>
-            <button className="text-light flex w-fit items-center gap-2 rounded-xl bg-sand p-1 font-plexSerif">
-              <p className="text-xs">For Business Partner</p>
+            <div className="flex w-fit items-center gap-2 rounded-xl bg-sand p-1 font-plexSerif text-light">
+              <ButtonLink buttonName={"For Business Partner"} />
               <RightArrowOneIcon />
-            </button>
+            </div>
           </div>
         </div>
       </div>
