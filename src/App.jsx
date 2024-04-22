271e0abd373bb32205e4d03ce4c715579e240596
Cyloon <cyloon@gmail.com>
2024-04-22 03:22:29 +0200

    component is working just need visual polishing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import Partners from "./components/Partners";
-
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="">
+      <Header />
       <Partners />
     </div>
   );
---
+++ src/components/Header.jsx
@@ -0,0 +1,108 @@
+import React, { useState } from "react";
+
+const navlinks = [
+  {
+    title: "WHAT WE DO",
+    link: "/whatwedo",
+  },
+  {
+    title: "HOW IT WORKS",
+    link: "/howitworks",
+  },
+  {
+    title: "PROJECT",
+    link: "/project",
+  },
+  {
+    title: "ABOUT",
+    link: "/about",
+  },
+  {
+    title: "CONTACT",
+    link: "/contact",
+  },
+  {
+    title: "PARTNER",
+    link: "/partner",
+  },
+  {
+    title: "INDIVIDUAL",
+    link: "/individual",
+  },
+];
+
+const Header = () => {
+  const [open, setopen] = useState(false);
+
+  const handleMenu = () => {
+    setopen((prev) => !prev);
+  };
+  return (
+    <div className={open ? "bg-orange-700" : {}}>
+      <div className="mx-auto px-4 font-bold sm:px-6 lg:px-8">
+        <div className="flex h-16 items-center justify-between">
+          <div className="item-center flex">
+            <a href="/">
+              {open ? (
+                <img src="/images/yp_logo_white.svg" alt="Logo" />
+              ) : (
+                <img src="/images/yp_logo_brown.svg" alt="Logo" />
+              )}
+            </a>
+          </div>
+          {/* navlinks */}
+          <div className="hidden md:block">
+            <div className="ml-10 flex items-baseline space-x-4">
+              {navlinks.map((link, index) => (
+                <a
+                  key={index}
+                  className={
+                    link.title === "INDIVIDUAL"
+                      ? "font-bold text-orange-700 transition-all duration-500 hover:opacity-45"
+                      : "font-bold text-black transition-all duration-500 hover:opacity-45"
+                  }
+                  href={link.link}
+                >
+                  {link.title}
+                </a>
+              ))}
+            </div>
+          </div>
+          {/* hamburger button */}
+          <div className="-mr-2 flex md:hidden">
+            <button
+              type="button"
+              onClick={handleMenu}
+              className="inline-flex items-center justify-center p-2"
+            >
+              <span className="sr-only">Open Main Menu</span>
+              {open == true ? "BACK" : "MENU"}
+            </button>
+          </div>
+        </div>
+      </div>
+      {/* mobile-menu */}
+      {open ? (
+        <div className="md:hidden">
+          <div className="ox-2 right-0 h-screen w-screen items-center justify-between space-y-1 bg-orange-700 pb-0 pt-2 text-center sm:px-3">
+            {navlinks.map((link, index) => (
+              <a
+                key={index}
+                className={
+                  link.title === "INDIVIDUAL"
+                    ? "block px-3 py-2 font-bold text-white transition-all duration-500"
+                    : "block px-3 py-2 font-bold text-black transition-all duration-500"
+                }
+                href={link.link}
+              >
+                {link.title}
+              </a>
+            ))}
+          </div>
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export default Header;
