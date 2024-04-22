2bfcad1a5aa1576ec739eadf88e521a88a54144a
marianordqvist <marianordqvist@live.se>
2024-04-22 14:50:45 +0200

    add: WWD to homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,15 @@
-import Hero from '../components/Hero';
+import Hero from "../components/Hero";
 import Partners from "../components/Partners";
 import ProjectCarouselSection from "../components/Projects/ProjectCarouselSection";
 import ReadyToTakeAction from "../components/ReadyToTakeAction";
 import NewsSection from "../components/NewsSection";
+import WWDSections from "../components/WWDSections";
 
 const HomePage = () => {
   return (
     <>
       <Hero />
+      <WWDSections />
       <ProjectCarouselSection />
       <Partners />
       <NewsSection />
