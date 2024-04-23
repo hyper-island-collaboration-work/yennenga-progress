0f77a05faf1aed03bf98f5fd948f3661623a11ac
Kseniia Tikhonova <116882002+TikhonovaKs@users.noreply.github.com>
2024-04-23 13:52:19 +0200

    hotfix: fix env file (#15)

--- src/client.js
+++ src/client.js
@@ -1,13 +1,6 @@
 import * as contentful from "contentful";
-// REACT_APP_SPACE_ID='1mn42d90w2s9'
-// REACT_APP_ACCESS_TOKEN='jqojvZwwiLq_FmThUnOfO_ET4QzOMGdtbLvGAlmNP7w'
-
-// export const client = contentful.createClient({
-//   space: process.env.REACT_APP_SPACE_ID,
-//   accessToken: process.env.REACT_APP_ACCESS_TOKEN,
-// });
 
 export const client = contentful.createClient({
-    space: '1mn42d90w2s9',
-    accessToken: 'jqojvZwwiLq_FmThUnOfO_ET4QzOMGdtbLvGAlmNP7w',
-  });
+  space: import.meta.env.VITE_SPACE_ID,
+  accessToken: import.meta.env.VITE_ACCESS_TOKEN,
+});
