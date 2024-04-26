39ce4bea03c5422acee1c7aef27f9f417d730247
Maria Nordqvist <144018694+marianordqvist@users.noreply.github.com>
2024-04-26 14:34:05 +0200

    Fix: NewsSection reusable component (#31)
    * fix:change NewsSection to DynamicListSection
    
    * Fix:correct-bg-color

---
+++ src/components/DynamicListSection.jsx
@@ -0,0 +1,33 @@
+import ButtonLink from "./ButtonLink";
+
+export default function NewsSection({
+  title,
+  content,
+  linkText,
+  linkPath,
+  renderItem,
+}) {
+  return (
+    <>
+      <section className="news-section flex flex-col justify-between  bg-stone-200 p-12 md:flex-row">
+        <div className="flex-item-1 flex flex-row items-center justify-between md:items-start">
+          <h3 className="mb-10 font-plexSerif text-2xl font-bold">{title}</h3>
+          <div className="all-news-link-top pb-7 md:hidden">
+            <ButtonLink buttonName={linkText} path={linkPath} />
+          </div>
+        </div>
+        <div className="flex-item-2 text-base md:w-2/4">
+          <ul className="divide-y-[2px] divide-gray-400 border-t-[2px] border-gray-400">
+            {content.map((item) => (
+              <li key={item.id}>{renderItem(item)}</li>
+            ))}
+          </ul>
+
+          <div className="all-news-link-bottom hidden pt-7 md:block">
+            <ButtonLink buttonName={linkText} path={linkPath} />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,17 +2,31 @@ import Hero from "../components/Hero";
 import Partners from "../components/Partners";
 import ProjectCarousel from "../components/ProjectList/ProjectCarousel";
 import ReadyToTakeAction from "../components/ReadyToTakeAction";
-import NewsSection from "../components/NewsSection";
 import WWDSections from "../components/WWDSections";
+import DynamicListSection from "../components/DynamicListSection";
+import useContentful from "../providers/ContentfulProvider/ContentfulProvider.hook";
+import NewsPost from "../components/NewsPost";
 
 const HomePage = () => {
+  //data from context to newsSection
+  const { newsPosts } = useContentful();
+  const threeLatestNewsPosts = newsPosts.slice(0, 5);
+
   return (
     <>
       <Hero />
       <WWDSections />
       <ProjectCarousel />
       <Partners />
-      <NewsSection />
+      <DynamicListSection
+        title="News"
+        content={threeLatestNewsPosts}
+        linkText="All News"
+        linkPath="/newspage"
+        renderItem={(post) => (
+          <NewsPost key={post.id} post={post} showFullPost={false} />
+        )}
+      />
       <ReadyToTakeAction />
     </>
   );
