2ec70fa7bfdf15888a596cced1ae3549c008e4f8
Maria Nordqvist <144018694+marianordqvist@users.noreply.github.com>
2024-05-01 12:18:17 +0200

    fix:env (#46)

--- src/client.js
+++ src/client.js
@@ -5,8 +5,7 @@ import * as contentful from "contentful";
 //   accessToken: import.meta.env.VITE_ACCESS_TOKEN,
 // });
 
-
 export const client = contentful.createClient({
-  space: '1mn42d90w2s9',
-  accessToken: 'jqojvZwwiLq_FmThUnOfO_ET4QzOMGdtbLvGAlmNP7w',
+  space: import.meta.env.VITE_SPACE_ID,
+  accessToken: import.meta.env.VITE_ACCESS_TOKEN,
 });
